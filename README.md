601e84849e238b9dd96f18af6f1e90898490fbc7
Michael Pope <map7777@gmail.com>
2010-07-01 17:11:41 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+#About
+
+**jQuery Depeche Mode**
+
+Allows a user to select items from a list with number keys on the keyboard 0 to 9, where 0 equals 10.
+
+Once the line is highlighted then you can hit the following keys to click the link:
+s = Show
+e = Edit
+d = Destroy
+t = Tag
+
+All these links must be ajax links so that the click function works.  You might want to use my jQuery.Beeline plugin.
+
+#Requires
+
+jquery-1.4.2
+
+#Recommend
+
+jquery.beeline
+
+#Use
+
---
+++ jquery.depechemode.js
@@ -0,0 +1,47 @@
+/*
+ * jQuery depechemode (Shortcut keys plugin)
+ *
+ * Based upon plugin
+ * http://github.com/jeresig/jquery.hotkeys
+ *
+ * Special thanks to Andrew France.
+ *
+*/
+(function($){
+    var select_keys = ['s', 'e', 'd', 't'];
+
+    $.fn.depechemode = function(options){
+	$(this).ready(function(event){
+
+	    // Allow selecting through keyboard
+	    $(this).keydown(function(e) {
+
+		key = String.fromCharCode(e.which);
+
+		// Check if a number was entered between 0 and 9
+		if (key >= 0 && key <= 9){
+		    no = key - 1;
+		    $('tr.listing').removeClass('highlight');
+		    $('tr.listing:eq('+no+')').addClass('highlight');
+		}else{
+		    key = key.toLowerCase();
+		    
+		    // else initiate the show/edit/destroy/tag link for that selection
+		    if (select_keys.indexOf(key) >= 0){
+			switch (key){
+			case 's':
+			    $('.highlight').children('td').children('a:contains(Show)').click();
+			    break;
+			case 'e':
+			    $('.highlight').children('td').children('a:contains(Edit)').click();
+			    break;
+			case 'd':
+			    $('.highlight').children('td').children('a:contains(Destroy)').click();
+			    break;
+			};
+		    };
+		};
+	    });
+	});
+    };
+})(jQuery);
